90a425f339a7320ad6010a29100cf18abc5d42cd
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-02-22 14:39:06 +0530

    Used Css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import NetflixSeries from "./components/NetflixSeries";
-// import "./components/Netflix.css";
-import './components/Netflix.module.css'
+import "./index.css";
+import styles from "./components/Netflix.module.css";
 
 function App() {
   return (
     <section className="container">
-      <h1 className="card-heading">List of Best Netflix Series</h1>
+      <h1 className={styles["card-heading"]}>List of Best Netflix Series</h1>
       <NetflixSeries />
     </section>
   );
--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,9 +1,13 @@
+import styles from "./Netflix.module.css";
+import styled from "styled-components";
+
 export const SeriesCard = ({ data }) => {
   const { id, img_url, name, rating, genre, cast, watch_url, description } =
     data;
 
-  const ratingClass = rating >= 8.5 ? "super_hit" : "average";
-  const ratingHoverClass = rating >= 8.5 ? "super_hit_hover " : "average_hover";
+  const ratingClass = rating >= 8.5 ? styles["super-hit"] : styles.average;
+  const ratingHoverClass =
+    rating >= 8.5 ? styles["super-hit-hover"] : styles["average-hover"];
 
   const btn_style = {
     fontSize: "1.6rem",
@@ -14,16 +18,28 @@ export const SeriesCard = ({ data }) => {
     cursor: "pointer",
     fontWeight: "bold",
   };
+
+  // const Button = styled.button`
+  //   color: green;
+  // `;
+
+  // const Button = styled.button({
+  //   color: "white",
+  //   backgroundColor: "red",
+  // });
   return (
-    <li className="card">
+    <li className={styles.card}>
       <div>
-        <img src={img_url} alt={name} className="series-card-img" />
+        <img src={img_url} alt={name} className={styles["series-card-img"]} />
       </div>
-      <div className="card-content">
+      <div className={styles["card-content"]}>
         <h2>Name : {name}</h2>
-        <h3 className="rating">
+        <h3 className={styles.rating}>
           rating :
-          <span className={`rating_score ${ratingClass}`}>{rating}</span>
+          <span className={`${styles["rating-score"]} ${ratingClass}`}>
+            {" "}
+            {rating}
+          </span>
         </h3>
 
         <p>summary: {description}</p>
